1c284cd0391da96e11cf31044053a1d65b6178ab
ozawa <1261276386@qq.com>
2021-12-30 23:54:27 +0800

    ref属性

--- scaffolding/README.md
+++ scaffolding/README.md
@@ -25,3 +25,16 @@
     1. vue.js是完整版的Vue，包含：核心功能 + 模板解析器。
     2. vue.runtime.xxx.js是运行版的Vue，只包含：核心功能；没有模板解析器。
 2. 因为vue.runtime.xxx.js没有模板解析器，所以不能使用template这个配置项，需要使用render函数接收到的createElement函数去指定具体内容。
+
+## vue.config.js配置文件
+
+1. 使用vue inspect > output.js可以查看到Vue脚手架的默认配置。
+2. 使用vue.config.js可以对脚手架进行个性化定制，详情见：https://cli.vuejs.org/zh 并点击配置栏
+
+## ref属性
+
+1. 被用来给元素或子组件注册引用信息（id的替代者）
+2. 应用在html标签上获取的是真实DOM元素，应用在组件标签上是组件实例对象（vc）
+3. 使用方式：
+   1. 打标识：```<h1 ref="xxx">.....</h1>``` 或 ```<School ref="xxx"></School>```
+   2. 获取：```this.$refs.xxx```
---
+++ scaffolding/src/ref/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <!-- 对于标签来说，ref和id其实没啥太大区别 -->
+    <h2 ref="msg">{{msg}}</h2>
+    <!-- 但是对于组件来说就很有用了，可以直接获取组件vc -->
+    <Student ref="student"></Student>
+    <button @click="printRef">点击输出真实DOM</button>
+  </div>
+</template>
+
+<script>
+import Student from './components/Student'
+
+export default {
+  name: "App",
+  components: {
+    Student
+  },
+  data() {
+    return {
+      msg: '开始学习ref'
+    }
+  },
+  methods: {
+    // 打印真实DOM
+    printRef() {
+      // 这里打印的是标签的真实dom
+      console.log('h2的真实DOM', this.$refs.msg)
+      // 这里其实是打印的是Student的vc
+      console.log('Student组件', this.$refs.student)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
