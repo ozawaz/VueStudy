8869b1ffea8bc2005d61ecfac599f6cbfa598507
Ozawa <1261276386@qq.com>
2021-12-31 11:46:26 +0800

    TodoList案例-静态页面

--- scaffolding/src/main.js
+++ scaffolding/src/main.js
@@ -1,21 +1,17 @@
 import Vue from 'vue'
 // 引入App
-import App from './plugin/App.vue'
-// 引入混合
-import mixin from "@/props/mixin";
-// 引入插件
-import plugins from "@/plugin/plugins";
+import App from './todolist/App.vue'
+// // 引入混合
+// import mixin from "@/props/mixin";
+// // 引入插件
+// import plugins from "@/plugin/plugins";
 
 /* 关闭生产提示 */
 Vue.config.productionTip = false
-/* 全局注册混合 */
-Vue.mixin(mixin)
-/* 使用插件 */
-Vue.use(plugins)
-
-function Extend() {
-
-}
+// /* 全局注册混合 */
+// Vue.mixin(mixin)
+// /* 使用插件 */
+// Vue.use(plugins)
 
 new Vue({
   render: h => h(App),
---
+++ scaffolding/src/todolist/components/List.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul class="todo-main">
+    <Item/>
+    <Item/>
+    <Item/>
+    <Item/>
+  </ul>
+</template>
+
+<script>
+import Item from "@/todolist/components/Item";
+
+export default {
+  name: "List",
+  components: {
+    Item
+  }
+}
+</script>
+
+<style scoped>
+  /*main*/
+  .todo-main {
+    margin-left: 0;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0;
+  }
+
+  .todo-empty {
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+  }
+</style>
